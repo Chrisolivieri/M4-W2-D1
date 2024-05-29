bbc900497850259cf4ad4d73685fbc1414e7792e
Chrisolivieri <christian.olivieri93@gmail.com>
2024-05-29 19:25:09 +0200

    update next prev button

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 //             console.log(cani)
 
 //             let card = ""
-            
+
 //             cani.photos.map((item, i) => {
 //                 card += ` <div class="card" style="width: 18rem;">
 //                 <img src= ${item.src.medium} class="card-img-top" alt="...">
@@ -26,7 +26,7 @@
 //                   <li class="list-group-item"><b>URL fotografo</b>: ${item.photographer_url}</li>
 //                   <li class="list-group-item"><b>ID foto</b>: ${item.id}</li>
 //                 </ul>
-                
+
 //               </div> `
 
 
@@ -35,25 +35,30 @@
 //         })
 // })
 
-function search(){
+let page = 1
+
+function search() {
 
   let img = document.getElementById("searchImg").value
+  
+ 
 
-  fetch("https://api.pexels.com/v1/search/?page=1&locale=it-IT&per_page=16&query=" + img, {
+
+  fetch(`https://api.pexels.com/v1/search/?page=${page}&locale=it-IT&per_page=16&query=` + img, {
     method: "GET",
     headers: {
-        "authorization": "QVKoZAeN3Ctag1Vi7ZVgnV5zkkCWAujAwPf5Lxn21rpbYqvi0QYKHVzq",
-        "content-type": "application/json"
+      "authorization": "QVKoZAeN3Ctag1Vi7ZVgnV5zkkCWAujAwPf5Lxn21rpbYqvi0QYKHVzq",
+      "content-type": "application/json"
     }
-}).then((response) => {
+  }).then((response) => {
     response.json()
-        .then((data) => {
-            console.log(data)
-            
-            let card = ""
-            
-            data.photos.map((item, i) => {
-                card += ` <div class="card">
+      .then((data) => {
+        console.log(data)
+
+        let card = ""
+
+        data.photos.map((item, i) => {
+          card += ` <div class="card">
                 <img src= ${item.src.medium} class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title"><b>Descrizione</b></h5>
@@ -68,19 +73,125 @@ function search(){
               </div> `
 
 
-            })
-            document.getElementById("searchTitle").innerHTML = `<h2>Risultati ricerca per: ${img}</h2>`
-            document.getElementById("card").innerHTML = card
-
-            
-            
         })
-})
+
+        document.getElementById("searchTitle").innerHTML = `<h2>Risultati ricerca per: ${img}</h2>`
+        document.getElementById("prevPage").innerHTML = `<button onclick=prevPage()>Prev page</button>`
+        document.getElementById("nextPage").innerHTML = `<button onclick=nextPage()>Next page</button>`
+        document.getElementById("card").innerHTML = card
+
+
+
+
+      })
+  })
 
 }
 
-document.getElementById('searchImg').addEventListener('keypress', function(event) {
-  if (event.keyCode === 13) { 
+document.getElementById('searchImg').addEventListener('keypress', function (event) {
+  if (event.keyCode === 13) {
     search();
   }
 })
+
+function nextPage(){
+  page++
+
+  let img = document.getElementById("searchImg").value
+  
+ 
+
+
+  fetch(`https://api.pexels.com/v1/search/?page=${page}&locale=it-IT&per_page=16&query=` + img, {
+    method: "GET",
+    headers: {
+      "authorization": "QVKoZAeN3Ctag1Vi7ZVgnV5zkkCWAujAwPf5Lxn21rpbYqvi0QYKHVzq",
+      "content-type": "application/json"
+    }
+  }).then((response) => {
+    response.json()
+      .then((data) => {
+        console.log(data)
+
+        let card = ""
+
+        data.photos.map((item, i) => {
+          card += ` <div class="card">
+                <img src= ${item.src.medium} class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title"><b>Descrizione</b></h5>
+                  <p class="card-text">${item.alt}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><b>Fotografo</b>: ${item.photographer}</li>
+                  <li class="list-group-item"><b><a href = ${item.photographer_url} target="_blank">Pagina fotografo</a></b></li>
+                  <li class="list-group-item"><b>ID foto</b>: ${item.id}</li>
+                </ul>
+                
+              </div> `
+
+
+        })
+
+        document.getElementById("searchTitle").innerHTML = `<h2>Risultati ricerca per: ${img}</h2>`
+        document.getElementById("prevPage").innerHTML = `<button onclick=prevPage()>Prev page</button>`
+        document.getElementById("nextPage").innerHTML = `<button onclick=nextPage()>Next page</button>`
+        document.getElementById("card").innerHTML = card
+
+
+
+
+      })
+  })
+}
+
+function prevPage(){
+  page--
+
+  let img = document.getElementById("searchImg").value
+  
+ 
+
+
+  fetch(`https://api.pexels.com/v1/search/?page=${page}&locale=it-IT&per_page=16&query=` + img, {
+    method: "GET",
+    headers: {
+      "authorization": "QVKoZAeN3Ctag1Vi7ZVgnV5zkkCWAujAwPf5Lxn21rpbYqvi0QYKHVzq",
+      "content-type": "application/json"
+    }
+  }).then((response) => {
+    response.json()
+      .then((data) => {
+        console.log(data)
+
+        let card = ""
+
+        data.photos.map((item, i) => {
+          card += ` <div class="card">
+                <img src= ${item.src.medium} class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title"><b>Descrizione</b></h5>
+                  <p class="card-text">${item.alt}</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><b>Fotografo</b>: ${item.photographer}</li>
+                  <li class="list-group-item"><b><a href = ${item.photographer_url} target="_blank">Pagina fotografo</a></b></li>
+                  <li class="list-group-item"><b>ID foto</b>: ${item.id}</li>
+                </ul>
+                
+              </div> `
+
+
+        })
+
+        document.getElementById("searchTitle").innerHTML = `<h2>Risultati ricerca per: ${img}</h2>`
+        document.getElementById("prevPage").innerHTML = `<button onclick=prevPage()>Prev page</button>`
+        document.getElementById("nextPage").innerHTML = `<button onclick=nextPage()>Next page</button>`
+        document.getElementById("card").innerHTML = card
+
+
+
+
+      })
+  })
+}
